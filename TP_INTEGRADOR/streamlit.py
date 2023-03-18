078067692fdaff4fded2ae80417fd5dde40d75bb
Nicolas <nicolascolaci@hotmail.com>
2023-03-18 20:34:04 -0300

    test streamlit

--- TP_INTEGRADOR/streamlit.py
+++ TP_INTEGRADOR/streamlit.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import seaborn as sns
+from matplotlib import pyplot as plt
 import pickle
 
 st.title("Predicción de Temperatura")
 st.write("Esto es la primer prueba")
 
 #  levantamos el df_test
-with open('df_test.pkl', 'rb') as f_dftest:
+with open('TP_INTEGRADOR/df_test.pkl', 'rb') as f_dftest:
         dataset_test = pickle.load(f_dftest)
 
 dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est'])
