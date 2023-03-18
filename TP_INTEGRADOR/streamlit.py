963824194ed30d63d838204769a625468267f1f5
Nicolas <nicolascolaci@hotmail.com>
2023-03-18 16:49:13 -0300

    testing pickle

--- TP_INTEGRADOR/streamlit.py
+++ TP_INTEGRADOR/streamlit.py
@@ -4,3 +4,11 @@ import pickle
 st.title("Predicción de Temperatura")
 st.write("Esto es la primer prueba")
 
+#  levantamos el df_test
+with open('./df_test.pkl', 'rb') as f_dftest:
+        dataset_test = pickle.load(f_dftest)
+
+
+
+
+dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est'])
