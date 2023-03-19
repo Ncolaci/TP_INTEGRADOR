08006cfbf41093a4d1e6910f568735f1d87fe5dd
Nicolas <nicolascolaci@hotmail.com>
2023-03-18 21:00:20 -0300

    grafico

--- TP_INTEGRADOR/streamlit.py
+++ TP_INTEGRADOR/streamlit.py
@@ -10,7 +10,6 @@ st.write("Esto es la primer prueba")
 with open('TP_INTEGRADOR/df_test.pkl', 'rb') as f_dftest:
         dataset_test = pickle.load(f_dftest)
 
-grafico = plt.figure(figsize=(10,4))
-dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est'])
+grafico = dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est'])
 
 st.pyplot(grafico)
