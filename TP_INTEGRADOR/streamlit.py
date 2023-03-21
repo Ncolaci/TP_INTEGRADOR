edddb8f261a396b3964a32e729119e7dd5aab783
Nicolas <nicolascolaci@hotmail.com>
2023-03-20 22:14:29 -0300

    presentar

--- TP_INTEGRADOR/streamlit.py
+++ TP_INTEGRADOR/streamlit.py
@@ -9,15 +9,15 @@ st.title("Predicción de Temperatura")
 
 
 #  levantamos el df_test
-#with open('TP_INTEGRADOR/df_test.pkl', 'rb') as f_dftest:
-#        dataset_test = pickle.load(f_dftest)
+with open('TP_INTEGRADOR/df_test.pkl', 'rb') as f_dftest:
+        dataset_test = pickle.load(f_dftest)
 
-#st.write("Nuestro Dataset con las predicciones se ve asi")
+st.write("Nuestro Dataset con las predicciones se ve asi")
 #st.write(dataset_test)
 
 
 
-#fig = plt.figure(figsize=(15,15))
+fig = plt.figure(figsize=(15,15))
 
 st.pyplot(dataset_test.plot(kind = "line", y = ['temp_min', 'model_ARIMA','predict_est']).figure)
 
